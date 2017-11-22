4c94c848d14c117ce0cf5a909e0e7a434651f3e9
kh522 <kh522@cornell.edu>
2017-11-22 07:53:09 -0500

    added cnn stuff

---
+++ cnn.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from torch.autograd import Variable
+from torch import optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import argparse
+import os
+import shutil
+import time
+import torch.nn.functional as F
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+class basicCNN(nn.Module):
+	def __init__(self):
+			super(basicCNN,self).__init__()
+			self.conv1 = nn.Conv2d(3, 5, 3)
+			self.conv2 = nn.Conv2d(5, 10, 3)
+			self.fc1 = nn.Linear(640, 120)
+			self.fc2 = nn.Linear(120, 84)
+			self.fc3 = nn.Linear(84, 4)
+	
+	def forward(self, x):
+		out=F.max_pool2d(F.relu(self.conv1(x)),(2,2))
+		out=F.max_pool2d(F.relu(self.conv2(out)),2)
+		out=out.view(-1,self.num_flat_features(out))
+		out=F.relu(self.fc1(out))
+		out=F.relu(self.fc2(out))
+		out=self.fc3(out)
+		return out
+
+	def num_flat_features(self, x):
+		size = x.size()[1:]  # all dimensions except the batch dimension
+		num_features = 1
+		for s in size:
+			num_features *= s
+		return num_features
+
+
+net = basicCNN()
+#print(list(net.parameters()))
+params = list(net.parameters())
+print(len(params))
+print(params[0].size())
--- pytorch_cnn.py
+++ pytorch_cnn.py
@@ -1,41 +1,38 @@
 import numpy as np
 from scipy import misc
 import os
-import numpy as np
 import torch
 from torch.autograd import Variable
 from torch import optim
 from sklearn.datasets import load_digits
 import cnn
 import time
-# from keras.models import Sequential
-# from keras.layers import Activation,Conv2D,MaxPooling2D,Flatten
 
-# xTr=[]
-# current_class=0
-# yTr=[]
-# FILEPATH='ece5470project/re_FishImages/combine_image/'
-# for files in os.listdir(FILEPATH):
-# 	for image in os.listdir(FILEPATH+files):
-# 		img = misc.imread(FILEPATH+files+'/'+image)
-# 		# if xTr==[]:
-# 		# 	xTr=img
-# 		# else:
-# 		# 	xTr=np.concatenate((xTr,img),axis=)
-# 		xTr.append(img)
-# 		yTr.append(current_class)
-# 	current_class+=1
+xTr=[]
+current_class=0
+yTr=[]
+FILEPATH='ece5470project/re_FishImages/combine_image/'
+for files in os.listdir(FILEPATH):
+	for image in os.listdir(FILEPATH+files):
+		img = misc.imread(FILEPATH+files+'/'+image)
+		# if xTr==[]:
+		# 	xTr=img
+		# else:
+		# 	xTr=np.concatenate((xTr,img),axis=)
+		xTr.append(img)
+		yTr.append(current_class)
+	current_class+=1
 
-# with open ('Y.p','w') as f:
-# 	np.save(f,yTr)
+with open ('Y.p','w') as f:
+	np.save(f,yTr)
 
-# with open('X.p','w') as f:
-# 	np.save(f,xTr)
+with open('X.p','w') as f:
+	np.save(f,xTr)
 
 with open ('Y.p','r') as f:
 	trY = np.load(f)
 with open('X.p','r') as f:
-	trX = np.load(f)
+	trX = np.load(f).reshape(-1,3,40,40)
 
 n_examples,w,h,c = np.asarray(trX).shape
 p = np.random.permutation(n_examples)
@@ -54,6 +51,7 @@ trY=trY[:-test_length]
 
 print(teX.shape)
 print(trX.shape)
+n_examples= n_examples-test_length
 
 def counts(y):
 	c=[0,0,0,0]
@@ -68,7 +66,10 @@ teX = torch.from_numpy(teX).float()
 trY = torch.from_numpy(trY).long()
 teY = torch.from_numpy(teY).long()
 
-
+def predict(model, x_val):
+    x = Variable(x_val, requires_grad=False)
+    output = model.forward(x)
+    return output.data.numpy().argmax(axis=1)
 
 num_classes=4
 
@@ -93,20 +94,39 @@ def train(model, loss, optimizer, x_val, y_val):
 
 #model = build_model(n_features, n_classes)
 model=cnn.basicCNN()
-
+print(n_examples)
 loss = torch.nn.CrossEntropyLoss(size_average=True)
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-batch_size = 1
+optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.)
+batch_size = 50
 s=time.time()
 
-for i in range(100):
-	print('hello')
+#print trX
+#print trY
+
+for i in range(20):
 	cost = 0.
 	num_batches = n_examples // batch_size
 	for k in range(num_batches):
 		start, end = k * batch_size, (k + 1) * batch_size
+		if end > n_examples:
+			end=n_examples
+		#print start,end,n_examples,num_batches
 		cost += train(model, loss, optimizer,
 				trX[start:end], trY[start:end])
 	print("Epoch %d, cost = %f"
 		      % (i + 1, cost / num_batches))
 e=time.time()
+print("time =",e-s)
+res = predict(model,teX).reshape(-1,1)
+teY=teY.numpy()
+acc=0
+for i in range(test_length):
+	if res[i]==teY[i]:
+		acc+=1
+
+
+acc=acc*1.0/(test_length)
+print "accuracy ", acc
+print "batch size",batch_size
+print "optimizer", "Adam"
+print  "parameters", model.parameters
